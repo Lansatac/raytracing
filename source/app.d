4bda514e984cdc7606721041e854261482546ccf
Jason Yegge <Lansatac@gmail.com>
2019-09-08 10:24:03 -0600

    rendering to a window

--- source/app.d
+++ source/app.d
@@ -1,40 +1,104 @@
 import std.stdio;
+import std.algorithm;
+import std.range;
 
 import gl3n.linalg;
-import imaged;
 
 import sphere;
 import render;
+import pixel;
 
+import derelict.sdl2.sdl;
+import derelict.sdl2.image;
+
+enum uint width = 1920, height = 1080;
+//enum uint width = 640, height = 480;
+//enum uint width = 100, height = 100;
+//enum uint width = 64, height = 64;
+//enum uint width = 4, height = 4;
 
 int main(string[] args)
 {
-    enum uint width = 1024, height = 768;
-    //enum uint width = 100, height = 100;
-    //enum uint width = 4, height = 4;
-
+    alias vec3 = gl3n.linalg.vec3;
     sphere[] spheres;
     // position, radius, surface color, reflectivity, transparency, emission color
-    spheres ~= sphere.sphere(vec3( 0.0, -10004, -20), 10000, vec3(0.20, 0.20, 0.20), 0, 0.0, vec3(0.05));
-    spheres ~= sphere.sphere(vec3( 0.0,      0, -20),     4, vec3(1.00, 0.32, 0.36), 1, 0.5);
-    spheres ~= sphere.sphere(vec3( 5.0,     -1, -15),     2, vec3(0.90, 0.76, 0.46), .6, 0.0);
-    spheres ~= sphere.sphere(vec3( 5.0,      0, -25),     3, vec3(0.65, 0.77, 0.97), .4, 0.0);
-    spheres ~= sphere.sphere(vec3(-5.5,      0, -15),     3, vec3(0.90, 0.90, 0.90), .8, 0.0);
-    spheres ~= sphere.sphere(vec3(-2.5,      3, -30),     3, vec3(0.00, 0.90, 0.20), 0, 0.0);
+    spheres ~= sphere.sphere(vec3(0.0, -10004, -20), 10000, vec3(0.20, 0.20,
+            0.20), 0, 0.0, vec3(0.05));
+    spheres ~= sphere.sphere(vec3(0.0, 0, -20), 4, vec3(1.00, 0.32, 0.36), 1, 0.5);
+    spheres ~= sphere.sphere(vec3(5.0, -1, -15), 2, vec3(0.90, 0.76, 0.46), .6, 0.0);
+    spheres ~= sphere.sphere(vec3(5.0, 0, -25), 3, vec3(0.65, 0.77, 0.97), .4, 0.0);
+    spheres ~= sphere.sphere(vec3(-5.5, 0, -15), 3, vec3(0.90, 0.90, 0.90), .8, 0.0);
+    spheres ~= sphere.sphere(vec3(-2.5, 3, -30), 3, vec3(0.00, 0.90, 0.20), 0, 0.0);
     // light
-    spheres ~= sphere.sphere(vec3( 0.0,     20, -30),     3, vec3(0.00, 0.00, 0.00), 0, 0.0, vec3(3));
+    spheres ~= sphere.sphere(vec3(0.0, 20, -30), 3, vec3(0.00, 0.00, 0.00), 0, 0.0, vec3(3));
     auto pixels = render.render(spheres, width, height);
 
 
-    Image img = new Img!(Px.R8G8B8)(width, height);
+    DerelictSDL2.load();
 
-    foreach(i, pixel; pixels)
+    // Load the SDL2_image library.
+    DerelictSDL2Image.load();
+
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
-        import std.conv;
-        //writefln("r=%s g=%s b=%s", pixel.r, pixel.g, pixel.b);
-        img.setPixel(i % width, i / width, Pixel(to!int(pixel.r * 255), to!int(pixel.g * 255), to!int(pixel.b * 255)));
+        writefln("SDL_Init Error: %s", SDL_GetError());
+        return 1;
     }
-    img.write("image.png");
+    scope(exit) SDL_Quit();
 
+    SDL_Window* win = SDL_CreateWindow("raytracing", 100, 100, width, height, SDL_WINDOW_SHOWN);
+    if (win == null){
+        writefln("SDL_CreateWindow Error: %s", SDL_GetError());
+        SDL_Quit();
+        return 1;
+    }
+    scope(exit)SDL_DestroyWindow(win);
+
+    SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == null){
+        writefln("SDL_CreateRenderer Error: %s", SDL_GetError());
+        return 1;
+    }
+    scope(exit) SDL_DestroyRenderer(ren);
+
+    SDL_Surface* surface = SDL_CreateRGBSurfaceFrom(pixels.map!PackedPixelRGBA.array.ptr,
+        width, height,
+        32, width * uint.sizeof,
+        0xFF000000, 0x00FF0000, 0x0000FF00, 0x000000FF);
+    if (surface == null)
+    {
+        writefln("SDL_LoadBMP Error: %s", SDL_GetError());
+        return 1;
+    }
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, surface);
+    SDL_FreeSurface(surface);
+    if (tex == null){
+        writefln("SDL_CreateTextureFromSurface Error: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Event e;
+    bool quit = false;
+    while (!quit){
+        while (SDL_PollEvent(&e)){
+            if (e.type == SDL_QUIT){
+                quit = true;
+            }
+            if (e.type == SDL_KEYDOWN){
+                quit = true;
+            }
+            if (e.type == SDL_MOUSEBUTTONDOWN){
+                quit = true;
+            }
+        }
+        //First clear the renderer
+        // SDL_RenderClear(ren);
+        //Draw the texture
+        SDL_RenderCopy(ren, tex, null, null);
+        //Update the screen
+        SDL_RenderPresent(ren);
+        
+        SDL_Delay(1);
+    }
     return 0;
 }
---
+++ source/pixel.d
@@ -0,0 +1,30 @@
+
+import gl3n.linalg;
+
+unittest
+{
+    import std.format : format;
+
+    assert(PackedPixelRGBA(vec3(0,0,1)) == 0x0000FFFF,
+        format("Expected %#X, but it was %#X", 0x0000FFFF, PackedPixel(vec3(0,0,1))));
+    assert(PackedPixelRGBA(vec3(0,1,0)) == 0x00FF00FF);
+    assert(PackedPixelRGBA(vec3(1,0,0)) == 0xFF0000FF);
+    assert(PackedPixelRGBA(vec3(1,1,1)) == 0xFFFFFFFF);
+    assert(PackedPixelRGBA(vec3(0,0,0)) == 0x000000FF);
+}
+
+//Packs a vector3 into an ARGB int
+uint PackedPixelRGBA(vec3 color)
+{
+    return PackedPixelRGBA(vec4(color.r, color.g, color.b, 1f));
+}
+
+uint PackedPixelRGBA(vec4 color)
+{
+    uint red = cast(ubyte)(color.r * (ubyte.max));
+    uint green = cast(ubyte)(color.g * (ubyte.max));
+    uint blue = cast(ubyte)(color.b * (ubyte.max));
+    uint alpha = cast(ubyte)(color.a * (ubyte.max));
+    
+    return (alpha << 0) | (blue << 8) | (green << 16) | (red << 24);
+}
